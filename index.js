fd59884a5e86740d89dd1aaa70d102f37acf920a
Lyuda Barabash <barabashlyuda@gmail.com>
2017-12-26 18:50:21 +0100

    2nd working version of the game is ready

--- index.js
+++ index.js
@@ -13,20 +13,37 @@ function createRandomNum() {
 
 let sumScoreOne = 0;
 let sumScoreTwo = 0;
+let clickCounter = 0;
+
+function checkIfActive() {
+    if (playerOne.classList.contains('active')) {
+        playerOne.classList.remove('active')
+    } else if (playerTwo.classList.contains('active')) {
+        playerTwo.classList.remove('active');
+    }
+}
 
 function endGame() {
     if (sumScoreOne === 11) {
         gameOver.innerHTML = 'Game over. Winner is Player 1';
         buttonServe.removeEventListener('click', showResult);
+        checkIfActive();
     } else if (sumScoreTwo === 11) {
         gameOver.innerHTML = 'Game over. Winner is Player 2';
         buttonServe.removeEventListener('click', showResult);
+        checkIfActive();
     }
 }
 
 function showResult() {
+    clickCounter += 1;
+    if (clickCounter > 1) {
+        clickCounter = 0;
+        playerOne.classList.toggle('active');
+        playerTwo.classList.toggle('active');
+    }
+
     let random = createRandomNum();
-    console.log(random);
     if (random === 0) {
         sumScoreTwo += 1;
         scoreTwo.innerHTML = sumScoreTwo;
@@ -42,6 +59,12 @@ function resetGame() {
     scoreOne.innerHTML = 0;
     sumScoreOne = 0;
     sumScoreTwo = 0;
+    gameOver.innerHTML = '';
+    if (playerTwo.classList.contains('active')) {
+        playerTwo.classList.remove('active');
+    }
+    playerOne.classList.add('active');
+    buttonServe.addEventListener('click', showResult);
 }
 
 buttonServe.addEventListener('click', showResult);
